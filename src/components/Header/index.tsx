c2b6b1372670b845ef52d4e18cf0053e54f06845
matheusOliv23 <matheushenri1996@gmail.com>
2023-05-21 18:35:19 -0300

    feat: add initial header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="px-10 py-8">
+      <p className="text-3xl font-bold">Insightfull</p>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
